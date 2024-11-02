60c41cba23e5c7c4a44f181d3b5d0b519f64aa6d
Takakusagi Kazushi <kazushi-takakusagi@TakakusaginoMacBook-Pro.local>
2024-11-02 15:06:59 +0900

    feat: home dog

--- dog-focus-app/src/components/DogAnimation.tsx
+++ dog-focus-app/src/components/DogAnimation.tsx
@@ -6,20 +6,18 @@ export const Happy = () => (
         // src="/images/dog_animation.gif"
         src="/images/animation/happy.png"
         className="w-full h-full"
-        alt="Dog Animation"
+        alt="Dog Happy"
       />
     </div>
   );
 
-// videoパターン
-// export const DogAnimation = () => (
-//   <div className="w-48 h-48">
-//     <video
-//       src="/videos/dog_animation.mp4"
-//       className="w-full h-full"
-//       autoPlay
-//       loop
-//       muted
-//     />
-//   </div>
-// );
+export const Home = () => (
+    <div className="w-48 h-48">
+      <img
+        // src="/images/dog_animation.gif"
+        src="/images/animation/home.png"
+        className="w-full h-full"
+        alt="Dog Home"
+      />
+    </div>
+  );
